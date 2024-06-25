3c4d489ac954edff062087fb2bcf11760ffdc489
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2024-06-25 19:41:56 +0530

    Hello World with native code

--- src/root.zig
+++ src/root.zig
@@ -33,6 +33,45 @@ fn next_token(input: *[]const u8) ?Token {
     return .{ .word = input.*[0..next_whitespace] };
 }
 
+const CodePtr = *const fn(*VM) void;
+const Pointer = union {
+  native_code: CodePtr,
+  threaded_code: WordPtr,
+};
+const WordPtr = [*]const Pointer;
+/// A virtual machine that can compile and run code
+const VM = struct {
+  const Dictionary = std.StringHashMap(WordPtr);
+  run_dict: Dictionary,
+  code_alloc: std.heap.ArenaAllocator,
+
+  pub fn init(alloc: std.mem.Allocator) VM {
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    return .{ .run_dict = Dictionary.init(arena.allocator()), .code_alloc = arena };
+  }
+  pub fn execute(vm: *VM, word: WordPtr) void {
+    word[0].native_code(vm);
+  }
+  pub fn deinit(vm: VM) void {
+    defer vm.code_alloc.deinit();
+  }
+
+  fn hello_world(vm: *VM) void {
+    _ = vm;
+    std.debug.print("Hello World", .{});
+  }
+  test "Basic execution" {
+    var vm = VM.init(std.heap.page_allocator);
+    const code = [_]Pointer{Pointer { .native_code = &hello_world }};
+    vm.execute(&code);
+    defer vm.deinit();
+  }
+};
+
+test  {
+  _ = VM;
+}
+
 test "Tokenizing strings" {
     var input: []const u8 = "let x be 10 then print x then print 'Hello world!'";
     try std.testing.expectEqualStrings("let", next_token(&input).?.word);
