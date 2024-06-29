f02ac08b6eba4447e1b10d2136f995f1a55f0de1
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2024-06-29 19:26:55 +0530

    feat: Hello world from `docol`

--- src/root.zig
+++ src/root.zig
@@ -19,9 +19,9 @@ fn next_token(input: *[]const u8) ?Token {
     if (input.len == 0) {
         return null;
     }
-    if(input.*[0] == '\'' or input.*[0] == '"') {
-      const next_quote = (std.mem.indexOf(u8, input.*[1..], input.*[0..1]) orelse input.len) + 1;
-      return .{ .string = input.*[1..next_quote] };
+    if (input.*[0] == '\'' or input.*[0] == '"') {
+        const next_quote = (std.mem.indexOf(u8, input.*[1..], input.*[0..1]) orelse input.len) + 1;
+        return .{ .string = input.*[1..next_quote] };
     }
     const next_whitespace = std.mem.indexOfAny(u8, input.*, &std.ascii.whitespace) orelse input.len;
     defer input.* = input.*[next_whitespace..];
@@ -33,43 +33,88 @@ fn next_token(input: *[]const u8) ?Token {
     return .{ .word = input.*[0..next_whitespace] };
 }
 
-const CodePtr = *const fn(*VM) void;
+// ///////////////////////////////
+// Built in instructions
+// ///////////////////////////////
+
+fn docol(vm: *VM) void {
+    vm.curr_word = vm.curr_word.? + 1;
+    vm.execute(vm.curr_word.?[0].threaded_code);
+}
+fn hello_world(vm: *VM) void {
+    _ = vm.stdout.write("Hello world\n") catch {
+        @panic("IO Error");
+    };
+}
+
+// ///////////////////////////////
+// Virtual Machine
+// ///////////////////////////////
+
+const CodePtr = *const fn (*VM) void;
 const Pointer = union {
-  native_code: CodePtr,
-  threaded_code: WordPtr,
+    native_code: CodePtr,
+    threaded_code: WordPtr,
 };
 const WordPtr = [*]const Pointer;
+
 /// A virtual machine that can compile and run code
 const VM = struct {
-  const Dictionary = std.StringHashMap(WordPtr);
-  run_dict: Dictionary,
-  code_alloc: std.heap.ArenaAllocator,
+    const Dictionary = std.StringHashMap(WordPtr);
+    run_dict: Dictionary,
+    code_alloc: std.heap.ArenaAllocator,
+    curr_word: ?WordPtr,
+    stdout: std.io.AnyWriter,
 
-  pub fn init(alloc: std.mem.Allocator) VM {
-    var arena = std.heap.ArenaAllocator.init(alloc);
-    return .{ .run_dict = Dictionary.init(arena.allocator()), .code_alloc = arena };
-  }
-  pub fn execute(vm: *VM, word: WordPtr) void {
-    word[0].native_code(vm);
-  }
-  pub fn deinit(vm: VM) void {
-    defer vm.code_alloc.deinit();
-  }
+    pub fn init(alloc: std.mem.Allocator) VM {
+        var arena = std.heap.ArenaAllocator.init(alloc);
+        return .{ .run_dict = Dictionary.init(arena.allocator()), .code_alloc = arena, .curr_word = null, .stdout = std.io.getStdOut().writer().any() };
+    }
+    pub fn initialize_words(vm: *VM) !void {
+        try vm.run_dict.put("hello_world", &[_]Pointer{.{ .native_code = &hello_world }});
+    }
+    pub fn execute(vm: *VM, word: WordPtr) void {
+        vm.curr_word = word;
+        word[0].native_code(vm);
+    }
+    pub fn deinit(vm: VM) void {
+        defer vm.code_alloc.deinit();
+    }
 
-  fn hello_world(vm: *VM) void {
-    _ = vm;
-    std.debug.print("Hello World", .{});
-  }
-  test "Basic execution" {
-    var vm = VM.init(std.heap.page_allocator);
-    const code = [_]Pointer{Pointer { .native_code = &hello_world }};
-    vm.execute(&code);
-    defer vm.deinit();
-  }
+    pub fn compile_expr(vm: *VM, input: *[]const u8) !WordPtr {
+        _ = input;
+        var compile_target = std.ArrayList(Pointer).init(vm.code_alloc.allocator());
+        try compile_target.append(.{ .native_code = docol });
+        return compile_target.items.ptr;
+    }
+
+    test "Basic execution" {
+        var vm = VM.init(std.heap.page_allocator);
+        defer vm.deinit();
+        try vm.initialize_words();
+
+        var output = std.ArrayList(u8).init(std.testing.allocator);
+        defer output.deinit();
+        vm.stdout = output.writer().any();
+
+        const code = [_]Pointer{ .{ .native_code = &docol }, .{ .threaded_code = vm.run_dict.get("hello_world").? } };
+        vm.execute(&code);
+
+        try std.testing.expectEqualStrings("Hello world\n", output.items);
+    }
+    // test "Basic compilation" {
+    // var vm = VM.init(std.heap.page_allocator);
+    // defer vm.deinit();
+
+    // var code: []const u8 = "hello_world";
+    // const word = try vm.compile_expr(&code);
+    // try std.testing.expectEqualDeep(&[_]Pointer{.{ .native_code = &docol }}, word);
+    // vm.execute(word);
+    // }
 };
 
-test  {
-  _ = VM;
+test {
+    _ = VM;
 }
 
 test "Tokenizing strings" {
